840e61ab956456975a46dc40bd3b1a8ffe5c3dcb
Ruben Schmidmeister <ruben@myelin.ch>
2019-12-17 10:23:17 +0100

    Fix download url

--- .travis/install-grcov.sh
+++ .travis/install-grcov.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-version=0.5.5
+version="0.5.5"
 archive_name="grcov-linux-x86_64"
 archive_file="$archive_name.tar.bz2"
-download_url="https://github.com/mozilla/grcov/releases/download/$version/$archive_file"
+download_url="https://github.com/mozilla/grcov/releases/download/v$version/$archive_file"
 temp_directory=$(mktemp -d)
 executable_name=grcov
 
